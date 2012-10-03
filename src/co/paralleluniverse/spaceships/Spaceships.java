480c89c728785666792d53466ecd40bab34aefb1
pron <ron.pressler@gmail.com>
2012-10-04 00:53:48 +0200

    first commit

---
+++ src/co/paralleluniverse/spaceships/Spaceships.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.paralleluniverse.spaceships;
+
+/**
+ *
+ * @author pron
+ */
+public class Spaceships {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("foo");
+    }
+    
+}
